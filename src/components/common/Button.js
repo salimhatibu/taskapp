d23f185e8eb30f5b7615a1e9173adf23af4ab41e
Salim <salim.hatibu@strathmore.edu>
2025-09-21 15:25:15 +0300

    Deleted unnecessary files

--- src/components/common/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ children, onClick, type = 'button', className = '', ...props }) => (
-  <button type={type} className={`btn ${className}`} onClick={onClick} {...props}>
-    {children}
-  </button>
-);
-
-export default Button; 
--- src/components/common/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as Modal } from './Modal';
-export { default as Loader } from './Loader';
-export { default as Input } from './Input'; 
