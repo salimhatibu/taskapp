ce6f52b85480c20c73b7cdb386c866c66c4e90cc
Salim <salim.hatibu@strathmore.edu>
2025-09-22 07:37:19 +0300

    .

---
+++ backend/storage/framework/views/61d3613b7230f2579ff995563124ee5457684a9f.php
@@ -0,0 +1,4 @@
+<svg class="block h-9 w-auto fill-current text-gray-800" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"/>
+</svg>
+<?php /**PATH C:\xampp\htdocs\taskapp\backend\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
